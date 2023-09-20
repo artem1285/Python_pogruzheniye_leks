5a9374093b0a5d6037dcd5221349c5f46423ed2b
Artem Alekseev <artem_1285@mail.ru>
2023-09-20 20:42:20 +0500

    lekc_6

--- lekc_6.py
+++ lekc_6.py
@@ -23,7 +23,7 @@ print(*p, sep='\n')
 print(rnd.randint(1, 6))
 print(path) # NameError: name 'path' is not defined
 print(sys.path) # NameError: name 'sys' is not defined
-'''
+
 # Плохой импорт со звесдочкой
 
 from random import randint
@@ -41,9 +41,46 @@ def func(a: int, b: int) -> str:
     return z
 
 result = func(1, 6)
+'''
 
+#  Виды модулей
+"""Four basic mathematical operations.
+Addition, subtraction, multiplication and division as functions.
+"""
+_START_SUM = 0
+_START_MULT = 1
+_BEGINNING = 0
+_CONTINUATION = 1
 
+def add(*args):
+    res = _START_SUM
+    for item in args:
+        res += item
+    return res
 
+def sub(*args):
+    res = args[_BEGINNING]
+    for item in args[_CONTINUATION:]:
+        res -= item
+    return res
+
+def mul(*args):
+    res = _START_MULT
+    for item in args:
+        res *= item
+    return res
+
+def div(*args):
+    res = args[_BEGINNING]
+    for item in args[_CONTINUATION:]:
+        res /= item
+    return res
+
+print(f'{add(2, 4) = }')
+print(f'{add(2, 4, 6, 8) = }')
+print(f'{sub(10, 2) = }')
+print(f'{mul(2, 2, 2, 2, 2) = }')
+print(f'{div(-100, 5, -2) = }')
 
 
 
